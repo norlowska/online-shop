6e6dc0e8d129e38ac709e0c98682d31336894c3b
Unknown <19gures97@gmail.com>
2019-01-28 19:09:46 +0100

    Koszyk doprowadzony do porządku

--- OnlineShop/Migrations/Configuration.cs
+++ OnlineShop/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace OnlineShop.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(OnlineShop.Models.ApplicationDbContext context)
